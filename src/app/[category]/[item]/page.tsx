40c4a99026b9f5d4eafea5c1bb97c144e003f0ac
kiloooooooo <kilotango37@icloud.com>
2023-07-20 17:55:09 +0900

    一覧ページのOpenGraphなど

--- src/app/[category]/[item]/page.tsx
+++ src/app/[category]/[item]/page.tsx
@@ -1,5 +1,38 @@
-import PaginationNavigator from "@/components/pagination_navigator";
-import ArticleList from "@/components/article_list";
+import { Metadata } from 'next'
+import PaginationNavigator from "@/components/pagination_navigator"
+import ArticleList from "@/components/article_list"
+
+export async function generateMetadata({ params }: { params: { category: string, item: string } }): Promise<Metadata> {
+    const cat = params.category
+    const title = (
+        cat === 'all'
+            ? 'すべての記事'
+            : cat === 'art'
+                ? 'イラスト'
+                : cat === 'tech'
+                    ? '技術'
+                    : ''
+    ) + ' - KiLog'
+    const description = 'イラストと備忘録置き場'
+
+    return {
+        title: title,
+        twitter: {
+            card: 'summary',
+            title: title,
+            description: description,
+            creator: '@nola_mk2'
+        },
+        openGraph: {
+            title: title,
+            description: description,
+            url: '/',
+            siteName: 'KiLog',
+            locale: 'ja_JP',
+            type: 'website'
+        }
+    }
+}
 
 function Dummy({ params }: { params: { category: string, item: string } }) {
     return <ArticleList page={parseInt(params.item)} filterCategory={params.category === 'all' ? null : params.category}/>
