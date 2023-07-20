05c490cb1233c1810317860749b60941ca9b07b3
kiloooooooo <kilotango37@icloud.com>
2023-07-20 14:56:48 +0900

    リンク画像

--- src/app/article/[dirname]/page.tsx
+++ src/app/article/[dirname]/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Head from 'next/head'
 import { marked } from 'marked'
 import { getPost } from '@/app/lib/api'
 import { NotFound } from '@/components/not_found'
@@ -19,25 +20,34 @@ export default function Article({ params }: { params: { dirname: string } }) {
     const contentHtml = marked.parse(post.markdown)
     return (
         // <div className={'flex flex-col pl-4 pr-4 pt-8 pb-8 rounded-2xl surface-variant on-surface-variant-text'}>
-        <FadeSlide>
-            <div className={'flex flex-col'}>
-                <div className={'w-full'}>
-                    <BreadCrumb hierarchy={[
-                        {
-                            title: 'ホーム',
-                            href: '/'
-                        },
-                        {
-                            title: `${post.properties.datetime} の記事`,
-                            href: null
-                        },
-                    ]}/>
+        <div>
+            <Head>
+                <meta property={"twitter:image"} content={post.properties.leadingImagePath}/>
+                <meta property={"twitter:card"} content={"summary_large_image"}/>
+                <meta property={"twitter:title"} content={post.properties.title}/>
+                <meta property={"og:image"} content={post.properties.leadingImagePath}/>
+                <meta property={"og:title"} content={post.properties.title}/>
+            </Head>
+            <FadeSlide>
+                <div className={'flex flex-col'}>
+                    <div className={'w-full'}>
+                        <BreadCrumb hierarchy={[
+                            {
+                                title: 'ホーム',
+                                href: '/'
+                            },
+                            {
+                                title: `${post.properties.datetime} の記事`,
+                                href: null
+                            },
+                        ]}/>
+                    </div>
+                    <div className={'mb-8 rounded-2xl'}>
+                        <ArticleProps postProps={post.properties} />
+                    </div>
+                    <div className={'article-markdown-zone'} dangerouslySetInnerHTML={{ __html: contentHtml }}/>
                 </div>
-                <div className={'mb-8 rounded-2xl'}>
-                    <ArticleProps postProps={post.properties} />
-                </div>
-                <div className={'article-markdown-zone'} dangerouslySetInnerHTML={{ __html: contentHtml }}/>
-            </div>
-        </FadeSlide>
+            </FadeSlide>
+        </div>
     )
 }
